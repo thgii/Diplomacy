7c6ebff8ba2fe80c365fe14672caa444e04a4e1a
thgii <thomashgrahamii@gmail.com>
2025-09-30 14:10:28 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
+// src/App.jsx
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import SignInGate from "@/components/SignInGate";
-// import your pages...
+
+// Pages
+import GameLobby from "@/pages/GameLobby";   // <-- name matches component you render
+import GameBoard from "@/pages/GameBoard";
+import GameAdmin from "@/pages/GameAdmin";   // optional, if you actually have this page
 
 export default function App() {
   return (
     <SignInGate>
       <Routes>
-        {/* your routes */}
-        {/* <Route path="/" element={<Home/>} /> etc. */}
+        {/* Default: send "/" to /GameLobby */}
+        <Route path="/" element={<Navigate to="/GameLobby" replace />} />
+
+        {/* Actual pages */}
+        <Route path="/GameLobby" element={<GameLobby />} />
+        <Route path="/GameBoard" element={<GameBoard />} />
+        <Route path="/GameAdmin" element={<GameAdmin />} /> {/* remove if not needed */}
+
+        {/* Catch-all */}
+        <Route path="*" element={<Navigate to="/GameLobby" replace />} />
       </Routes>
     </SignInGate>
   );
